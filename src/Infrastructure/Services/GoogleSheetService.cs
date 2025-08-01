c9e12540c3b81ff07f643ec0bed0c7b566607dc7
Jacek Bukała <bukala.jacek@gmail.com>
2025-08-01 09:53:35 +0200

    2nd Push

---
+++ src/Infrastructure/Services/GoogleSheetService.cs
@@ -0,0 +1,32 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Application.Interfaces;
+
+public class GoogleSheetService : IGoogleSheetService
+{
+    private readonly SheetsService _sheetsService;
+    private readonly string _spreadsheetId;
+
+    public GoogleSheetService(string credentialsFilePath, string spreadsheetId)
+    {
+        _spreadsheetId = spreadsheetId;
+        GoogleCredential credential;
+        using var stream = new FileStream(credentialsFilePath, FileMode.Open, FileAccess.Read);
+        credential = GoogleCredential.FromStream(stream).CreateScoped(SheetsService.Scope.SpreadsheetsReadonly);
+
+        _sheetsService = new SheetsService(new BaseClientService.Initializer
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = "MyApp",
+        });
+    }
+
+    public async Task<IList<IList<object>>> ReadSheetAsync(string range)
+    {
+        var request = _sheetsService.Spreadsheets.Values.Get(_spreadsheetId, range);
+        var response = await request.ExecuteAsync();
+        return response.Values;
+    }
+}
