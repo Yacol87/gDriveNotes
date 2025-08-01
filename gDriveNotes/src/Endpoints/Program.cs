39fee5cb79324ee3e719a68117df1be7d59618c2
Jacek Bukała <bukala.jacek@gmail.com>
2025-08-01 08:21:21 +0200

    Swagger setup

--- gDriveNotes/src/Endpoints/Program.cs
+++ gDriveNotes/src/Endpoints/Program.cs
@@ -1,25 +1,34 @@
+using Application.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Set config values manually for demo purposes
+string credentialsFilePath = "local-disk-465710-p4-6cbf4b03bfa6.json"; // Put this in secrets later
+string spreadsheetId = "1w_ARyoN-IPqfNy0PPHCcYFfRaAQQzeyPZ5oZ4NrLHU0";
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Register GoogleSheetService
+builder.Services.AddScoped<IGoogleSheetService>(sp =>
+    new GoogleSheetService(credentialsFilePath, spreadsheetId));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+
+// Enable Swagger middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.MapGet("/", async (IGoogleSheetService googleSheets) =>
+{
+    var values = await googleSheets.ReadSheetAsync("Sheet1!A1:C5");
+    Console.Write(values);
 
-app.UseAuthorization();
-
-app.MapControllers();
-
+    return Results.Ok(values);
+});
 app.Run();
