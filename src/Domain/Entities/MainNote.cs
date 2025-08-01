ad4effe4a738cc4ef8e84df16173132f83036d93
Jacek Bukała <bukala.jacek@gmail.com>
2025-08-01 11:20:33 +0200

    Csproj restoration

---
+++ src/Domain/Entities/MainNote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gDriveNotes.Domain.Entities
+{
+    public class MainNote
+    {
+        public int Id { get; set; }
+        public string NoteContent { get; set; }
+        public int NoteArea { get; set; }
+        public int SenderId { get; set; }
+
+        public MainNote(int id, string noteContent, int noteArea, int senderId)
+        {
+            Id = id;
+            NoteContent = noteContent;
+            NoteArea = noteArea;
+            SenderId = senderId;
+        }
+        public MainNote() { }
+    }
+}
