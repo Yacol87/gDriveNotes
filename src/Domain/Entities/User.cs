d5398d708c0e0b6c88d80bd4cc81cae40193ce05
Jacek Bukała <bukala.jacek@gmail.com>
2025-08-01 12:12:55 +0200

    User entity

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public User() { }
+        private User(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+    }
+}
